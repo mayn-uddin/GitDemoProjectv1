7ed89bc31f9dab487073f7ed17b1cba32dc28a7c
mayn-uddin <mohammaduddin10@gmail.com>
2020-10-17 23:40:13 -0400

    commit the test2 file

---
+++ src/GitDemoTest2.java
@@ -0,0 +1,20 @@
+public class GitDemoTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		String hungry = "yes";
+		if (hungry == "yes" ||hungry == "Y" || hungry =="y")
+		
+		System.out.println ("eat sonthing");
+		
+		else {
+			
+			System.out.println ("Do work.....");
+		}
+	
+
+
+	}
+
+}
